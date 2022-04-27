a05b19963091411f7393c296c30d72df2c01a4f9
foam60 <antoine.mousse@gmail.com>
2022-04-27 12:29:04 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,134 @@
 #!/bin/bash
+local hostname='host100'
+local timezone='Europe/Paris'
+local keymap='fr'
+local name='user1'
+local name2='user2'
+local password='test1'
+local passwordRoot='root1'
+local packages=''
+# General utilities/libraries
+packages+=' alsa-utils aspell-en chromium cpupower gvim mlocate net-tools ntp openssh p7zip pkgfile powertop python python2 rfkill rsync sudo unrar unzip wget zip systemd-sysvcompat zsh grml-zsh-config'
+
+#Root and Swap partition
 loadkeys fr-latin9;
 parted /dev/sda mklabel msdos;
 parted /dev/sda mkpart primary ext4 0% 90%;
-parted /dev/sda mkpart primary linux-swapm  90% 100%;
-mkfs.ext4 /dev/sda1;
-mkswap /dev/sda2;
-mount /dev/sda1;
-genfstab -U -p /mnt >> /mnt/etc/fstab;
-arch-chroot /mnt;
-reboot;
-# lsblk to list all parts
+parted /dev/sda mkpart primary linux-swapm 90% 100%;
+mkfs.ext4 /dev/sda1
+mkswap /dev/sda2
+mount /dev/sda1
+genfstab -U -p /mnt/etc/fstab
+arch-chroot /mnt
+#Packages Install
+pacman -Sy --noconfirm $packages
+#Setting hostname
+echo "$hostname" > /etc/hostname
+#Setting timezone
+ln -sT "/usr/share/zoneinfo/$timezone" /etc/localtime
+#Setting local
+echo 'LANG="fr_FR.UTF-8"' >> /etc/locale.conf
+echo 'LC_COLLATE="C"' >> /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+#Setting Keymap
+echo "KEYMAP=$keymap" > /etc/vconsole.conf
+#Setting sudoers for 'wheel' group
+set_sudoers
+#Set root password
+echo -en "$passwordRoot\n$passwordRoot" | passwd
+#Setting user 1
+useradd -m -s /bin/zsh -G adm,systemd-journal,wheel,rfkill,games,network,video,audio,optical,floppy,storage,scanner,power,adbusers,wireshark "$name"
+echo -en "$password\n$password" | passwd "$name"
+#Setting user 2
+useradd -m -s /bin/zsh -G adm,systemd-journal,wheel,rfkill,games,network,video,audio,optical,floppy,storage,scanner,power,adbusers,wireshark "$name"
+echo -en "$password\n$password" | passwd "$name2"
+reboot
+
+#Function for seting up sudoers file
+set_sudoers() {
+    cat > /etc/sudoers <<EOF
+## sudoers file.
+##
+## This file MUST be edited with the 'visudo' command as root.
+## Failure to use 'visudo' may result in syntax or file permission errors
+## that prevent sudo from running.
+##
+## See the sudoers man page for the details on how to write a sudoers file.
+##
+##
+## Host alias specification
+##
+## Groups of machines. These may include host names (optionally with wildcards),
+## IP addresses, network numbers or netgroups.
+# Host_Alias	WEBSERVERS = www1, www2, www3
+##
+## User alias specification
+##
+## Groups of users.  These may consist of user names, uids, Unix groups,
+## or netgroups.
+# User_Alias	ADMINS = millert, dowdy, mikef
+##
+## Cmnd alias specification
+##
+## Groups of commands.  Often used to group related commands together.
+# Cmnd_Alias	PROCESSES = /usr/bin/nice, /bin/kill, /usr/bin/renice, \
+# 			    /usr/bin/pkill, /usr/bin/top
+##
+## Defaults specification
+##
+## You may wish to keep some of the following environment variables
+## when running commands via sudo.
+##
+## Locale settings
+# Defaults env_keep += "LANG LANGUAGE LINGUAS LC_* _XKB_CHARSET"
+##
+## Run X applications through sudo; HOME is used to find the
+## .Xauthority file.  Note that other programs use HOME to find   
+## configuration files and this may lead to privilege escalation!
+# Defaults env_keep += "HOME"
+##
+## X11 resource path settings
+# Defaults env_keep += "XAPPLRESDIR XFILESEARCHPATH XUSERFILESEARCHPATH"
+##
+## Desktop path settings
+# Defaults env_keep += "QTDIR KDEDIR"
+##
+## Allow sudo-run commands to inherit the callers' ConsoleKit session
+# Defaults env_keep += "XDG_SESSION_COOKIE"
+##
+## Uncomment to enable special input methods.  Care should be taken as
+## this may allow users to subvert the command being run via sudo.
+# Defaults env_keep += "XMODIFIERS GTK_IM_MODULE QT_IM_MODULE QT_IM_SWITCHER"
+##
+## Uncomment to enable logging of a command's output, except for
+## sudoreplay and reboot.  Use sudoreplay to play back logged sessions.
+# Defaults log_output
+# Defaults!/usr/bin/sudoreplay !log_output
+# Defaults!/usr/local/bin/sudoreplay !log_output
+# Defaults!/sbin/reboot !log_output
+##
+## Runas alias specification
+##
+##
+## User privilege specification
+##
+root ALL=(ALL) ALL
+## Uncomment to allow members of group wheel to execute any command
+%wheel ALL=(ALL) ALL
+## Same thing without a password
+# %wheel ALL=(ALL) NOPASSWD: ALL
+## Uncomment to allow members of group sudo to execute any command
+# %sudo ALL=(ALL) ALL
+## Uncomment to allow any user to run sudo if they know the password
+## of the user they are running the command as (root by default).
+# Defaults targetpw  # Ask for the password of the target user
+# ALL ALL=(ALL) ALL  # WARNING: only use this together with 'Defaults targetpw'
+%rfkill ALL=(ALL) NOPASSWD: /usr/sbin/rfkill
+%network ALL=(ALL) NOPASSWD: /usr/bin/netcfg, /usr/bin/wifi-menu
+## Read drop-in files from /etc/sudoers.d
+## (the '#' here does not indicate a comment)
+#includedir /etc/sudoers.d
+EOF
+
+    chmod 440 /etc/sudoers
+}
